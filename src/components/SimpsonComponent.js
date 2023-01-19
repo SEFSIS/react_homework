309841b5a0a9802d12f5c6e8cd0dfd51ecf03047
SEFSIS <sofiagryn@icloud.com>
2023-01-19 14:43:14 +0200

    hm1_

--- src/components/SimpsonComponent.js
+++ src/components/SimpsonComponent.js
@@ -9,7 +9,7 @@ export default function SimpsonComponent(props){
             <img src={pic}/>
             <h3> Biography</h3>
             <p>{itemInfo}</p>
-            <h3> Favourite phrases</h3>
+            <h3> Favourite phrases </h3>
             <p>{quote1}</p>
             <p>{quote2}</p>
             <p>{quote3}</p>
